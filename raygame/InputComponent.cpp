7c36ec53ba25df8c3a2662be50f74c2cc91440c4
Devin Broussard <devin.ray.broussard@gmail.com>
2021-12-20 07:32:34 -0600

    Renamed keys in input Component

--- raygame/InputComponent.cpp
+++ raygame/InputComponent.cpp
@@ -9,23 +9,18 @@ InputComponent::InputComponent(Actor* owner) :
 MathLibrary::Vector2 InputComponent::getMoveAxis()
 {
 	//Get the direction of the individual axis
-	float xDirection = -RAYLIB_H::IsKeyDown(KEY_A) + RAYLIB_H::IsKeyDown(KEY_D);
-	float yDirection = RAYLIB_H::IsKeyDown(KEY_S) - RAYLIB_H::IsKeyDown(KEY_W);
+	float xDirection = -RAYLIB_H::IsKeyDown(m_left) + RAYLIB_H::IsKeyDown(m_right);
+	float yDirection = RAYLIB_H::IsKeyDown(m_down) - RAYLIB_H::IsKeyDown(m_up);
 
 	//Return a new vector representing the move directiodn
 	return MathLibrary::Vector2(xDirection, yDirection).getNormalized();
 }
 
-void InputComponent::getAttackInput()
-{
-
-}
-
-MathLibrary::Vector2 getForwardAxis()
+MathLibrary::Vector2 InputComponent::getForwardAxis()
 {
 	//Get the direction of the individual axis
-	float xDirection = -RAYLIB_H::IsKeyDown(KEY_LEFT) + RAYLIB_H::IsKeyDown(KEY_RIGHT);
-	float yDirection = RAYLIB_H::IsKeyDown(KEY_DOWN) - RAYLIB_H::IsKeyDown(KEY_UP);
+	float xDirection = -RAYLIB_H::IsKeyDown(m_lookLeft) + RAYLIB_H::IsKeyDown(m_lookRight);
+	float yDirection = RAYLIB_H::IsKeyDown(m_lookDown) - RAYLIB_H::IsKeyDown(m_lookUp);
 
 	return MathLibrary::Vector2(xDirection, yDirection).getNormalized();
 }
