5c451b5aefb00d975e5428c40c5f7e2951363c58
Devin Broussard <devin.ray.broussard@gmail.com>
2022-01-23 18:58:30 -0600

    Added some comments to code

--- raygame/Character.h
+++ raygame/Character.h
@@ -15,9 +15,17 @@ public:
 	//decontructor for character
 	~Character() override;
 
+	/// <summary>
+	/// Returns the health component
+	/// </summary>
+	/// <returns></returns>
 	HealthComponent* getHealthComponent() { return m_healthComponent; }
 	float getSpeed() { return m_speed; }
 	void setSpeed(float speed) { m_speed = speed; }
+	/// <summary>
+	/// Returns whether or not the character is attacking
+	/// </summary>
+	/// <returns></returns>
 	bool getIsAttacking() { return m_attackComponent->getIsAttacking(); }
 	void setAttacking(bool value) { m_attackComponent->setIsAttacking(value); }
 
@@ -31,6 +39,9 @@ public:
 	void draw() override;
 
 private:
+	/// <summary>
+	/// The velocity that the character moves at
+	/// </summary>
 	MathLibrary::Vector2 m_velocity;
 	MoveComponent* m_moveComponent;
 	HealthComponent* m_healthComponent;
